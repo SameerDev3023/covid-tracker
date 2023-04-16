512c14cdad3a57dc5f7fb5ff4f79b969fdb0841b
sameer <sameerdeveloper05@gmail.com>
2023-04-16 14:01:07 +0530

    complete covid tracker react app code

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,21 @@
-import React from 'react';
-import CovidDataCard from './CovidDataCard'
+import React from "react";
+import { useEffect, useState } from "react";
+import CovidDataCard from "./CovidDataCard";
 
-const Card = () =>{
-return(
-    <CovidDataCard/>
-)
-}
+const Card = () => {
+  const [StateWiseData, setStateWiseData] = useState({});
+  const GetCovidData = async () => {
+    try {
+      const ApiUrl = await fetch("https://data.covid19india.org/data.json");
+      const data = await ApiUrl.json();
+      setStateWiseData(data.statewise[0]);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  useEffect(() => {
+    GetCovidData();
+  }, []);
+  return <CovidDataCard StateWiseData={StateWiseData} />;
+};
 export default Card;
--- src/components/CovidDataCard.js
+++ src/components/CovidDataCard.js
@@ -1,11 +1,61 @@
-import React from 'react';
+import React from "react";
+import "./ComponentsCss/CovidCard.css";
 
- const AddDataCard = () =>{
-return(
+const AddDataCard = (props) => {
+ const CovidData = props.StateWiseData
+  return (
     <React.Fragment>
-
-        <h2>Hello this is my covid react app </h2>
+      <div className="container">
+        <div className="main-heading bg-dark text-white ">Covid Tracker 🔴</div>
+        <div className="row">
+          <div className="column col-md-4">
+            <div className="card bg-success text-white">
+              <h3>Recovered</h3>
+              <p>{CovidData.recovered}</p>
+              <p>This is a crona recoverd cases</p>
+            </div>
+          </div>
+          <div className="column col-md-4 ">
+            <div className="card bg-danger text-white">
+              <h3>Deaths</h3>
+              <p>{CovidData.deaths}</p>
+              <p>Deaths of carona patiens </p>
+            </div>
+          </div>
+          <div className="column col-md-4">
+            <div className="card bg-warning text-white ">
+              <h3>Delta Recovered</h3>
+              <p>{CovidData.deltarecovered}</p>
+              <p>Covid Delta Recoverds</p>
+            </div>
+          </div>
+        </div>
+        <div className="row section2">
+          <div className="column col-md-4">
+            <div className="card bg-primary text-white">
+              <h3>Active</h3>
+              <p>{CovidData.active}</p>
+              <p>This is Active Cases</p>
+            </div>
+          </div>
+          <div className="column col-md-4">
+            <div className="card bg-secondary text-white">
+              <h3>Confirmed</h3>
+              <p>{CovidData.confirmed}</p>
+              <p>Confirmed Carona Cases
+              </p>
+            </div>
+          </div>
+          <div className="column col-md-4">
+            <div className="card bg-info text-white">
+              <h3>last updated time</h3>
+              <p>{CovidData.lastupdatedtime}</p>
+              <p>Last Data updated time</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </React.Fragment>
-)
-}
+  );
+};
 export default AddDataCard;
