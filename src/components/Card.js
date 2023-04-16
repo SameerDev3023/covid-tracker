9607383ed06311c926215ad4dd8127f4132b0a86
sameer <sameerdeveloper05@gmail.com>
2023-04-16 12:09:43 +0530

    making components folder and making basic changes

---
+++ src/components/Card.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import CovidDataCard from './CovidDataCard'
+
+const Card = () =>{
+return(
+    <CovidDataCard/>
+)
+}
+export default Card;
---
+++ src/components/CovidDataCard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+ const AddDataCard = () =>{
+return(
+    <React.Fragment>
+
+        <h2>Hello this is my covid react app </h2>
+    </React.Fragment>
+)
+}
+export default AddDataCard;
